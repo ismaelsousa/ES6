f939c6fd88abd9a78daf3c39b3c8f9820d0b8e1c
ismael <ismaelsousa1998@outlook.com>
2019-06-19 21:34:38 -0300

    making an interface to the gituhub api

--- src/main.js
+++ src/main.js
@@ -88,5 +88,118 @@
 
 // console.log(s(1,2));
 
+//####################### async await #######################
 
-alert('teste');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("OK") }, 2000);
+})
+
+// minhaPromise()
+//     .then(response => {
+//         console.log(response)
+//     })
+//     .catch(err=>{
+
+//     })
+
+// async function executaPromise(){
+//     //const response = await minhaPromise();
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+// executaPromise();
+
+
+
+//####################### axios #######################
+// import axios from "axios";
+
+// class Api {
+//     static async getUserInfo(username) {
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`)
+//             console.log(response);
+//         } catch (erro) {
+//             console.warn('erro na api');
+
+//         }
+//     }
+
+// }
+
+// Api.getUserInfo("ismaelsousa");
+import api from "./api";
+
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById("repo-form");
+        this.listEl = document.getElementById("repo-list");
+        this.inputEl = document.querySelector('input[name=repository]')
+
+        this.registerHandlers();
+
+
+    }
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0){
+            return;
+        }
+
+        const response = await api.get(`/repos/${repoInput}`)
+
+        const {name , description, html_url, owner: {avatar_url}} = response.data;
+        
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        })
+
+        this.inputEl.value = "";
+
+        this.render();
+       
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repor => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repor.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repor.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild((document.createTextNode(repor.description)));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('href', repor.html_url)
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild((document.createTextNode('acessar')));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
+    }
+}
+
+new App();
