ea999d80d4d5555991f576877a2a349e5bcbfbd2
ismael <ismaelsousa1998@outlook.com>
2019-06-20 15:44:33 -0300

    successfully completed the course from ES6 making front-end for github

--- src/main.js
+++ src/main.js
@@ -147,6 +147,19 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'))
+            loadingEl.setAttribute("id", "loading")
+
+            this.formEl.appendChild(loadingEl);
+
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+    
     async addRepository(event) {
         event.preventDefault();
 
@@ -156,20 +169,28 @@ class App {
             return;
         }
 
-        const response = await api.get(`/repos/${repoInput}`)
+        this.setLoading();
+        try{
+            const response = await api.get(`/repos/${repoInput}`)
 
-        const {name , description, html_url, owner: {avatar_url}} = response.data;
+            const {name , description, html_url, owner: {avatar_url}} = response.data;
+            
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            })
+    
+            this.inputEl.value = "";
+    
+            this.render();
+        } catch(erro){
+            alert("o repositorio não existe!")
+        }
+
+        this.setLoading(false);
         
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url
-        })
-
-        this.inputEl.value = "";
-
-        this.render();
        
     }
 
